98770cfed9e5c90a3b7dd93da71b9480ae034f0d
frbernad <frbernad@itba.edu.ar>
2022-08-24 14:37:32 -0300

    feat: assets

--- TP2/visualization/utils/parser_utils.py
+++ TP2/visualization/utils/parser_utils.py
@@ -1,5 +1,3 @@
-import math
-from math import pi
 from typing import List
 
 import numpy as np
@@ -9,22 +7,16 @@ from pandas import DataFrame
 
 
 def get_frame_particles(df: DataFrame):
-    ids = np.arange(1, len(df.x) + 1)
     particles = od.Particles()
-    particles.create_property('Particle Identifier', data=ids)
+    particles.create_property('Particle Identifier', data=df.id)
     particles.create_property('Position',
                               data=np.array((df.x, df.y, np.zeros(len(df.x)))).T)
     particles.create_property('Radius', data=df.radius)
-    particles.create_property('Angle', data=df.angle)
+    particles.create_property('angle', data=df.angle)
     particles.create_property('Force', data=np.array((np.cos(df.angle) * df.speed, np.sin(df.angle) * df.speed,
                                                       np.zeros(len(df.x)))).T)
 
     return particles
-
-
-def _generate_radius(R: float, x_offset: float, y_offset: float, n: int = 500):
-    return [[x_offset + math.cos(2 * pi / n * x) * R, y_offset + math.sin(2 * pi / n * x) * R, 0] for x in
-            range(0, n + 1)]
 
 
 def get_particles_data(static_file: str, flocks_file: str, particle_R: float) -> List[DataFrame]:
--- TP2/visualization/visualization_ovito.py
+++ TP2/visualization/visualization_ovito.py
@@ -29,7 +29,7 @@ def visualization_ovito(config_file: str):
     pipeline.modifiers.append(create_particle_pos)
 
     export_file(pipeline, '../results/visualization.dump', 'lammps/dump',
-                columns=["Particle Identifier", "Position.X", "Position.Y", "Position.Z", "Radius", "Angle",
+                columns=["Particle Identifier", "Position.X", "Position.Y", "Position.Z", "Radius", "angle",
                          "Force.X", "Force.Y", "Force.Z"],
                 multiple_frames=True, start_frame=0, end_frame=len(dfs) - 1)
 
