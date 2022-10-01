0dafaec05612db8ea52c3b79b2659ad35936e95f
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2022-10-01 00:33:12 -0300

    feat: Parse planet files
    Missing dump into static and dynamic file

--- TP4/simulator/src/main/java/ar/edu/itba/ss/simulator/utils/ParseUtils.java
+++ TP4/simulator/src/main/java/ar/edu/itba/ss/simulator/utils/ParseUtils.java
@@ -3,6 +3,7 @@ package ar.edu.itba.ss.simulator.utils;
 import ar.edu.itba.ss.simulator.utils.Particle.Position;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
@@ -66,6 +67,115 @@ public class ParseUtils {
         dynamicScanner.close();
 
         return new ParticlesParserResult(N, L, particlesPerTime);
+    }
+
+    public static void parsePlanets(final File earthFile, final File venusFile, final File sunFile) throws FileNotFoundException {
+
+        final Scanner earthScanner = new Scanner(earthFile);
+        final Scanner venusScanner = new Scanner(venusFile);
+        final Scanner sunScanner = new Scanner(sunFile);
+
+        final Map<String, List<String>> planetInfo = new HashMap<>();
+        final String earthName = earthFile.getName().split("_")[0];
+        final String venusName = venusFile.getName().split("_")[0];
+        final String sunName = sunFile.getName().split("_")[0];
+        planetInfo.putIfAbsent(earthName, new ArrayList<>());
+        planetInfo.putIfAbsent(venusName, new ArrayList<>());
+        planetInfo.putIfAbsent(sunName, new ArrayList<>());
+
+
+        while (earthScanner.hasNextLine()) {
+            final List<String> earthInfo = planetInfo.get(earthName);
+            final String line = earthScanner.nextLine();
+            if (line.toLowerCase().contains("vol. mean radius (km)")) {
+                String[] parts = line.split("=");
+                earthInfo.add(parts[1].trim().split(" ")[0].split("\\+")[0]);
+                earthInfo.add(parts[2].trim().split("\\+")[0] + parts[1].trim().split(" ")[4].replace("x10^", "E+"));
+            }
+
+            if (line.toLowerCase().contains("x =")) {
+                String[] positions = line.split("=");
+                earthInfo.add(positions[1].trim().split(" ")[0]);
+                earthInfo.add(positions[2].trim().split(" ")[0]);
+            }
+
+            if (line.toLowerCase().contains("vx=")) {
+                String[] velocities = line.split("=");
+                earthInfo.add(velocities[1].trim().split(" ")[0]);
+                earthInfo.add(velocities[2].trim().split(" ")[0]);
+                break;
+            }
+
+
+        }
+        earthScanner.close();
+
+        boolean foundMass = false;
+
+        while (venusScanner.hasNextLine()) {
+
+            final List<String> venusInfo = planetInfo.get(venusName);
+            final String line = venusScanner.nextLine();
+            if (line.toLowerCase().contains("vol. mean radius (km)")) {
+                venusInfo.add(line.split("=")[1].trim().split(" ")[0].split("\\+")[0]);
+
+            }
+
+            if (line.toLowerCase().contains("mass") && !foundMass) {
+                String[] massParts = line.split("=");
+                venusInfo.add(massParts[1].trim().split(" ")[0] + massParts[0].trim().split(" ")[1].replace("x10^", "E+"));
+                foundMass = true;
+            }
+
+            if (line.toLowerCase().contains("x =")) {
+                String[] positions = line.split("=");
+                venusInfo.add(positions[1].trim().split(" ")[0]);
+                venusInfo.add(positions[2].trim().split(" ")[0]);
+            }
+
+            if (line.toLowerCase().contains("vx=")) {
+                String[] velocities = line.split("=");
+                venusInfo.add(velocities[1].trim().split(" ")[0]);
+                venusInfo.add(velocities[2].trim().split(" ")[0]);
+                break;
+            }
+        }
+
+        venusScanner.close();
+
+        foundMass = false;
+
+        while (sunScanner.hasNextLine()) {
+
+            final List<String> sunInfo = planetInfo.get(sunName);
+            final String line = sunScanner.nextLine();
+            if (line.toLowerCase().contains("vol. mean radius, km")) {
+                sunInfo.add(line.split("=")[1].trim().split(" ")[0].replace("10^", "E+"));
+
+            }
+
+            if (line.toLowerCase().contains("mass") && !foundMass) {
+                String[] massParts = line.split("=");
+                sunInfo.add(massParts[2].trim().split("~")[1] + massParts[1].split(",")[1].trim().split(" ")[0]);
+                foundMass = true;
+            }
+
+            if (line.toLowerCase().contains("x =")) {
+                String[] positions = line.split("=");
+                sunInfo.add(positions[1].trim().split(" ")[0]);
+                sunInfo.add(positions[2].trim().split(" ")[0]);
+            }
+
+            if (line.toLowerCase().contains("vx=")) {
+                String[] velocities = line.split("=");
+                sunInfo.add(velocities[1].trim().split(" ")[0]);
+                sunInfo.add(velocities[2].trim().split(" ")[0]);
+                break;
+            }
+        }
+
+        sunScanner.close();
+
     }
 
     public static class ParticlesParserResult {
