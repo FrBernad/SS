fix: decimal precision

TP4/visualization/plots/ej1_ECM.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 
 from utils.parser_utils import get_particles_data
 
+
 # Estudiar como disminuye el error al disminuir el paso de integración (dt). Usar ejes semilogarítmicos
 # o logarítmicos para poder apreciar las diferencias de error a escalas pequeñas. ¿Cuál de
 # los esquemas de integración resulta mejor para este sistema ?
@@ -36,7 +37,7 @@ def plot_oscillator_ecm(run_files: List[Tuple]):
         event_times = np.array(list(map(lambda df: df.time, dfs)))
         particle_df = np.array(list(map(lambda df: df.data.x, dfs))).flatten()
 
-        ECMs.append(np.sum((particle_df - analytic_y) ** 2) / len(analytic_y))
+        ECMs.append(np.sum((analytic_y - particle_df) ** 2) / len(analytic_y))
 
         data.append(
             go.Scatter(
@@ -94,12 +95,13 @@ def plot_oscillator_ecm(run_files: List[Tuple]):
         data=go.Scatter(
             x=names,
             y=ECMs,
+            marker=dict(size=5)
         ),
         layout=go.Layout(
             title=dict(text=f'ECMs', x=0.5),
             xaxis=dict(title=r'$\Large{\text{Algoritmo}}$', exponentformat="power",
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
-            yaxis=dict(title=r'$\Large{\text{ECM (m)}}$', exponentformat="power", type='log',
+            yaxis=dict(title=r'$\Large{\text{ECM } (\text{m}^{\text{2}})}$', exponentformat="power", type='log',
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
             font=dict(
                 family="Computer Modern",
@@ -123,6 +125,7 @@ if __name__ == "__main__":
     results_gear_predictor = '../../results/ej1/resultsGearPredictor.txt'
     results_beeman = '../../results/ej1/resultsBeeman.txt'
     results_verlet_original = '../../results/ej1/resultsVerletOriginal.txt'
+
     plot_oscillator_ecm(
         [
             (static_file, results_verlet_original),

TP4/visualization/plots/ej1_dt.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 
 from utils.parser_utils import get_particles_data
 
+
 # En todos los casos graficar las soluciones analítica y numérica y calcular el error cuadrático
 # medio (sumando las diferencias al cuadrado para todos los pasos temporales y normalizando por el
 # número total de pasos).
@@ -26,7 +27,7 @@ def plot_oscillator_per_dt(run_folders: List[Tuple[str, str]]):
         print(f'''Parsing {names[i]}''')
         ECMs = []
         for j, file in enumerate(files):
-            print(f'''dt = {dts[j]}''')
+            print(f'''dt = {dts[j]}, step = {steps[j]}''')
             analytic_x = np.arange(0, 5 + dts[j], dts[j])[::steps[j]]
             exp_term = -(gamma / (2 * m))
             cos_term = ((k / m) - ((gamma ** 2) / (4 * m ** 2))) ** 0.5
@@ -36,13 +37,14 @@ def plot_oscillator_per_dt(run_folders: List[Tuple[str, str]]):
 
             particle_df = np.array(list(map(lambda df: df.data.x, dfs))).flatten()
 
-            ECMs.append(np.sum((particle_df - analytic_y) ** 2) / len(analytic_y))
+            ECMs.append(np.sum((analytic_y - particle_df) ** 2) / len(analytic_y))
 
         data.append(
             go.Scatter(
                 x=dts,
                 y=ECMs,
-                name=names[i]
+                name=names[i],
+                marker=dict(size=10)
             )
         )
 
@@ -51,26 +53,21 @@ def plot_oscillator_per_dt(run_folders: List[Tuple[str, str]]):
         layout=go.Layout(
             title=dict(text=f'Oscillator ECM per dt', x=0.5),
             xaxis=dict(title=r'$\Large{\text{dt (s)}}$', exponentformat="power", type='log',
+                       dtick=1,
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
-            yaxis=dict(title=r'$\Large{\text{ECM (m)}}$', exponentformat="power", type='log',
+            yaxis=dict(title=r'$\Large{\text{ECM } (\text{m}^{\text{2}})}$', exponentformat="power", type='log',
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
             font=dict(
                 family="Computer Modern",
                 size=22,
             ),
             plot_bgcolor='rgba(0,0,0,0)',
-            legend=dict(
-                yanchor="top",
-                y=0.99,
-                xanchor="right",
-                x=0.99
-            )
         )
 
     )
 
     # Set figure size
-    fig.update_layout(width=1000, height=1000)
+    fig.update_layout(width=1500, height=1000)
 
     fig.show()
 

TP4/visualization/plots/ej2_distance_to_venus.py
@@ -23,18 +23,28 @@ def plot_distance_to_venus(static_files: str, position_per_date_folder: str):
     min_distances = []
     date_strs = []
 
-    for file in position_per_date_files:
-        date_str = filename_to_date(file).strftime("%d-%m-%Y %H")
+    orbit_len = 1500 + 6052
+    step = 300 * 6
 
-        print(f'''Parsing {date_str}''')
+    for file in position_per_date_files:
+        date_str = filename_to_date(file).strftime("%d-%m-%Y")
+
+        print(f'''{datetime.now().strftime("%H:%M:%S")} - Parsing {date_str}''')
         date_strs.append(date_str)
 
-        dfs = get_particles_data(static_files, file, step=int(12 * 60 * 60 / 300))
+        dfs = get_particles_data(static_files, file)
 
         dfs_data = np.array(list(map(lambda df: df.data, dfs)))
         distances = np.sqrt((dfs_data[:, 2, 1] - dfs_data[:, 3, 1]) ** 2 + (dfs_data[:, 2, 2] - dfs_data[:, 3, 2]) ** 2)
+        min_distance = min(distances)
+        min_distances.append(min_distance)
+        # 23-10-2022 - 23-05-2023 - 23-02-2024 23-07-2024 23-01-2025 23-04-2025 23-08-2025
 
-        min_distances.append(min(distances))
+        print(
+            f'''  Min distance: {min_distance}km'''
+            f''' - Time:{np.where(distances == min_distance)[0][0] * step / (60 * 60 * 24)}''')
+        if min_distance < orbit_len:
+            print(f'''  Inside Orbit!!''')
 
     data = go.Scatter(
         x=date_strs,
@@ -44,7 +54,7 @@ def plot_distance_to_venus(static_files: str, position_per_date_folder: str):
         data=data,
         layout=go.Layout(
             title=dict(text=f'Min Distance to venus per launch date', x=0.5),
-            xaxis=dict(title=r'$\Large{\text{Dia de salida}}$',
+            xaxis=dict(title=r'$\Large{\text{Día de salida}}$',
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
             yaxis=dict(title=r'$\Large{\text{Distancia (km)}}$', exponentformat="power",
                        linecolor="#000000", ticks="outside", tickwidth=2, tickcolor='black', ticklen=10),
@@ -71,5 +81,5 @@ def plot_distance_to_venus(static_files: str, position_per_date_folder: str):
 
 if __name__ == "__main__":
     static_file = '../../assets/ej2/StaticPlanets'
-    dates_folder = '../../results/ej2/multipleRuns'
+    dates_folder = '../../results/ej2/multipleRuns/2years'
     plot_distance_to_venus(static_file, dates_folder)